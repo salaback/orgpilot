52cc2575262c4bde2b97ee7dfdc4f6c0bfce1eb3
Sean Alaback <sean.alaback@gmail.com>
2025-06-30 10:08:25 +0200

    Fix migration names

--- database/migrations/2025_06_29_155607_create_subscriptions_table.php
+++ database/migrations/2025_06_29_155607_create_subscriptions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('customers_id');
+            $table->foreignId('customer_id');
             $table->string('type');
             $table->string('stripe_id')->unique();
             $table->string('stripe_status');
